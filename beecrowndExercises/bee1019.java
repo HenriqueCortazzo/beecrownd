b06cacfb4d1af455aa17a4af596079862e824037
Henrique <Matheus@gg>
2023-12-23 09:00:40 -0300

    Resolvendo problema.

--- beecrowndExercises/bee1019.java
+++ beecrowndExercises/bee1019.java
@@ -1,7 +1,16 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1019 {
     public static void main(String[] args) {
-        
+        int horas, minutos, segundos, resto;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        segundos = n;
+        horas = segundos / 3600;
+        minutos = segundos / 60;
+        segundos = n -(minutos * 60);
+        System.out.printf("%d : %d : %d", horas, minutos, segundos);
     }
 }

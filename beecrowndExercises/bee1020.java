ba2888ef45ba5df5d09028608699c6f9a7a41fcb
HenriqueCortazzo <qdeusmeajude@gmail.com>
2023-12-23 10:54:22 -0300

    resolvendo bee 1020.

--- beecrowndExercises/bee1020.java
+++ beecrowndExercises/bee1020.java
@@ -1,8 +1,16 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1020 {
     public static void main(String[] args) {
-        int ano,mes,dia, valorEntrada;
-        
+        int ano, mes, dia, valorEntrada;
+        Scanner sc = new Scanner(System.in);
+        valorEntrada = sc.nextInt();
+        dia = valorEntrada;
+        ano = dia / 365;
+        mes = (dia - (ano * 365)) / 30;
+        dia = valorEntrada  - (mes * 30)- (ano * 365);
+        System.out.printf("%d anos(s)\n%d mes(es)\n%d dia(s)\n",ano,mes,dia);
     }
 }

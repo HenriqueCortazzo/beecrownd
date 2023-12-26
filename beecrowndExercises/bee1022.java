08d2e79e78108e80c4e450fcd736e88d0ed49b66
HenriqueCortazzo <qdeusmeajude@gmail.com>
2023-12-26 12:22:18 -0300

    Resolução bee1022

--- beecrowndExercises/bee1022.java
+++ beecrowndExercises/bee1022.java
@@ -1,7 +1,21 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1022 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, soma1, soma2;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        soma1 = c + d;
+        soma2 = a + b;
+        if (a > 0 && c > 0 && d > 0 && a % 2 == 0 && soma1 > soma2 && b > c && d > a) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
     }
 }

f8a130e989b3f1c803bb2606c33ef4df609cb7e6
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-02 12:17:19 -0300

    Correção de bug na condicional media<5

--- beecrowndExercises/bee1040.java
+++ beecrowndExercises/bee1040.java
@@ -5,32 +5,23 @@ import java.util.Scanner;
 public class bee1040 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n1, n2, n3, n4, media, notaExame, resultadoExame, mediaFinal;
+        float n1, n2, n3, n4, media, notaExame, mediaFinal;
 
-        n1 = sc.nextDouble();
-        n2 = sc.nextDouble();
-        n3 = sc.nextDouble();
-        n4 = sc.nextDouble();
+        n1 = sc.nextFloat();
+        n2 = sc.nextFloat();
+        n3 = sc.nextFloat();
+        n4 = sc.nextFloat();
 
-        int peso1, peso2, peso3, peso4, pesoTotal;
-
-        peso1 = 2;
-        peso2 = 3;
-        peso3 = 4;
-        peso4 = 1;
-        pesoTotal = peso1 + peso2 + peso3 + peso4;
-
-        media = ((n1 * peso1) + (n2 * peso2) + (n3 * peso3) + (n4 * peso4)) / pesoTotal;
-
-        System.out.printf("Media: %.1f\n", media);
+        media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
 
         if (media >= 7) {
+            System.out.printf("Media: %.1f\n", media);
             System.out.println("Aluno aprovado.");
+
         } else if (media >= 5 && media <= 6.9) {
-            System.out.println("Aluno em exame.");
-            notaExame = sc.nextDouble();
-            resultadoExame = (notaExame + media) / 2;
-            if (resultadoExame >= 5) {
+            notaExame = sc.nextFloat();
+            mediaFinal = (notaExame+media)/2;
+            if (mediaFinal >= 5) {
                 System.out.printf("Media: %.1f\n", media);
                 System.out.println("Aluno em exame.");
                 System.out.printf("Nota do exame: %.1f\n", notaExame);
@@ -39,9 +30,8 @@ public class bee1040 {
                 } else {
                     System.out.println("Aluno reprovado.");
                 }
-                mediaFinal = ((n1 * peso1) + (n2 * peso2) + (n3 * peso3) + (n4 * peso4) + notaExame) / pesoTotal;
                 System.out.printf("Media final: %.1f\n", mediaFinal);
-            } else if (resultadoExame < 5) {
+            } else if (mediaFinal < 5) {
                 System.out.printf("Media: %.1f\n", media);
                 System.out.println("Aluno em exame.");
                 if (notaExame >= 5) {
@@ -49,10 +39,10 @@ public class bee1040 {
                 } else {
                     System.out.println("Aluno reprovado.");
                 }
-                mediaFinal = ((n1 * peso1) + (n2 * peso2) + (n3 * peso3) + (n4 * peso4) + notaExame) / pesoTotal;
                 System.out.printf("Media final: %.1f\n", mediaFinal);
             }
         } else {
+            System.out.printf("Media: %.1f\n", media);
             System.out.println("Aluno reprovado.");
         }
     }

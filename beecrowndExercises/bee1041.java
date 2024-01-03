27a521f1203a32195dbf0506e761823990b3997a
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-03 12:38:39 -0300

    Criando condicionais

--- beecrowndExercises/bee1041.java
+++ beecrowndExercises/bee1041.java
@@ -1,2 +1,24 @@
-package beecrowndExercises;public class bee1041 {
+package beecrowndExercises;
+
+import java.util.Scanner;
+
+public class bee1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float x, y;
+        x = sc.nextFloat();
+        y = sc.nextFloat();
+
+        if (x == y && y == 0) {
+            System.out.println("Origem");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4   ");
+        }
+    }
 }

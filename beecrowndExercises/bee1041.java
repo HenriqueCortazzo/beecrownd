73d245cb2175b4ef275828e4454ab147d48ca5c9
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-03 12:48:12 -0300

    bee1041 100% concluído

--- beecrowndExercises/bee1041.java
+++ beecrowndExercises/bee1041.java
@@ -8,17 +8,22 @@ public class bee1041 {
         float x, y;
         x = sc.nextFloat();
         y = sc.nextFloat();
-
-        if (x == y && y == 0) {
-            System.out.println("Origem");
-        } else if (x > 0 && y > 0) {
-            System.out.println("Q1");
-        } else if (x < 0 && y > 0) {
-            System.out.println("Q2");
-        } else if (x < 0 && y < 0) {
-            System.out.println("Q3");
-        } else if (x > 0 && y < 0) {
-            System.out.println("Q4   ");
+        if (x > 0 && y == 0 || x < 0 && y == 0) {
+            System.out.println("Eixo X");
+        } else if (y > 0 && x == 0 || y < 0 && x == 0) {
+            System.out.println("Eixo Y");
+        } else {
+            if (x == y && y == 0) {
+                System.out.println("Origem");
+            } else if (x > 0 && y > 0) {
+                System.out.println("Q1");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Q2");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Q3");
+            } else if (x > 0 && y < 0) {
+                System.out.println("Q4");
+            }
         }
     }
 }

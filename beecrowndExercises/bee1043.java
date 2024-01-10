e16619db51ce2ec51617903a4fbdac1dddda656b
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-10 11:48:00 -0300

    Criação de condicional

--- beecrowndExercises/bee1043.java
+++ beecrowndExercises/bee1043.java
@@ -1,7 +1,17 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1043 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c;
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+        if (a < b + c && b < a + c && c < a + b){
+
+        }
     }
 }

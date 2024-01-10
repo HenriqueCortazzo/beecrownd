e616617f5af98661c724ebf6445856a58948578a
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-10 12:34:52 -0300

    Finalizando bee1044

--- beecrowndExercises/bee1044.java
+++ beecrowndExercises/bee1044.java
@@ -1,7 +1,17 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1044 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        if (b%a==0 || a%b==0)
+            System.out.println("Sao Multiplos");
+        else
+            System.out.println("Nao sao Multiplos");
+
     }
 }

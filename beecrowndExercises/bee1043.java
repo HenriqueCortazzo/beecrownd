f7219a68de6c811e488a74648eb1be0df1ac7cc0
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-10 11:51:01 -0300

    Adiciondo funcionalidade para validação de triângulos

--- beecrowndExercises/bee1043.java
+++ beecrowndExercises/bee1043.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class bee1043 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a, b, c;
+        double a, b, c, perimetro;
         a = scanner.nextDouble();
         b = scanner.nextDouble();
         c = scanner.nextDouble();
 
-        if (a < b + c && b < a + c && c < a + b){
-
+        if (a < b + c && b < a + c && c < a + b) {
+            perimetro = a + b + c;
+            System.out.printf("Perimetro = %.1f\n", perimetro);
+        }else{
+            
         }
     }
 }

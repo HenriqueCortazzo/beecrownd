5242d1f37d0aa4a115942d4dbe6020e03c3e8503
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-11 19:38:44 -0300

    Inicializando condicionais

--- beecrowndExercises/bee1045.java
+++ beecrowndExercises/bee1045.java
@@ -1,7 +1,18 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1045 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c, maior = 0;
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+        double[] vetor = {a, b, c};
+        for (double v : vetor) {
+            if (v > maior) maior = v;
+        }
+        a = maior;
     }
 }

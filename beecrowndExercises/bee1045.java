35e69707dfc437471e92192757c143b96a519e7c
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-11 19:48:21 -0300

    Adicionando funcionalidade

--- beecrowndExercises/bee1045.java
+++ beecrowndExercises/bee1045.java
@@ -14,5 +14,22 @@ public class bee1045 {
             if (v > maior) maior = v;
         }
         a = maior;
+
+        if (a>=b+c){
+            System.out.println("NAO FORMA TRIANGULO");
+        }
+        if (Math.pow(a,2)== Math.pow(b,2)+Math.pow(c,2)){
+            System.out.println("TRIANGULO RETANGULO");
+        } if (Math.pow(a,2)>(Math.pow(b,2)+Math.pow(c,2))){
+            System.out.println("TRIANGULO OBTUSANGULO");
+        } if (Math.pow(a,2)<Math.pow(b,2)+Math.pow(c,2)){
+            System.out.println("TRIANGULO ACUTANGULO");
+        }
+        if (a==b && b ==c){
+            System.out.println("TRIANGULO EQUILATERO");
+        }
+        if (a==b || b==c || a==c){
+            System.out.println("TRIANGULO ISOSCELES");
+        }
     }
 }

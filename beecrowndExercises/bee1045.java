2895eed39a6a8a3466ad803902fd08ff4a34f25e
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-12 04:29:17 -0300

    Finalizando bee1045

--- beecrowndExercises/bee1045.java
+++ beecrowndExercises/bee1045.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class bee1045 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a, b, c, maior = 0;
+        double a, b, c, maior = 0, temp;
         a = scanner.nextDouble();
         b = scanner.nextDouble();
         c = scanner.nextDouble();
@@ -13,23 +13,30 @@ public class bee1045 {
         for (double v : vetor) {
             if (v > maior) maior = v;
         }
+        temp = a;
         a = maior;
-
-        if (a>=b+c){
-            System.out.println("NAO FORMA TRIANGULO");
+        if (a == b) {
+            b = temp;
+        } else if (a == c) {
+            c = temp;
         }
-        if (Math.pow(a,2)== Math.pow(b,2)+Math.pow(c,2)){
+        if (a >= b + c) {
+            System.out.println("NAO FORMA TRIANGULO");
+        }else {
+        if (Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2)) {
             System.out.println("TRIANGULO RETANGULO");
-        } if (Math.pow(a,2)>(Math.pow(b,2)+Math.pow(c,2))){
+        }
+        if (Math.pow(a, 2) > (Math.pow(b, 2) + Math.pow(c, 2))) {
             System.out.println("TRIANGULO OBTUSANGULO");
-        } if (Math.pow(a,2)<Math.pow(b,2)+Math.pow(c,2)){
+        }
+        if (Math.pow(a, 2) < (Math.pow(b, 2) + Math.pow(c, 2))) {
             System.out.println("TRIANGULO ACUTANGULO");
         }
-        if (a==b && b ==c){
+        if (a == b && b == c) {
             System.out.println("TRIANGULO EQUILATERO");
         }
-        if (a==b || b==c || a==c){
+        if (a == b && b != c || a == c && c !=b|| b==c && c!=a) {
             System.out.println("TRIANGULO ISOSCELES");
-        }
+        }}
     }
 }

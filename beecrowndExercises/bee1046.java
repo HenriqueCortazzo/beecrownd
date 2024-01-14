70297087ef689877efaf55376a490f63e02601e2
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-14 12:00:06 -0300

    Inicialziando condicionais

--- beecrowndExercises/bee1046.java
+++ beecrowndExercises/bee1046.java
@@ -1,7 +1,20 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1046 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int horaInicial, horaFinal,horasDeJogo;
+        horaInicial = scanner.nextInt();
+        horaFinal = scanner.nextInt();
+        horasDeJogo=0;
+
+        if (horaInicial<horaFinal){
+            horasDeJogo = horaInicial-horaFinal;
+        }else if(horaFinal<horaInicial){
+            horasDeJogo = (horaFinal+24)-horaInicial;
+        }
+        System.out.println(horasDeJogo);
     }
 }

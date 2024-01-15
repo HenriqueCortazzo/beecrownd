6c678063503ae65970fd70146d2cd1f4aeb96c43
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-15 19:11:29 -0300

    Validando condicionais

--- beecrowndExercises/bee1047.java
+++ beecrowndExercises/bee1047.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class bee1047 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int horaInicial, minutoInicial, horaFinal, minutoFinal, horasEMinutosTotais, resultadoFinal;
+        int horaInicial, minutoInicial, horaFinal, minutoFinal, horasTotais, minutosTotais, resultadoFinalHoras, resultadoFinalMinutos, horasEMinutosResultantes;
+        resultadoFinalHoras = 0;
+        resultadoFinalMinutos = 0;
+        String formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, resultadoFinalMinutos);
         horaInicial = scanner.nextInt();
         minutoInicial = scanner.nextInt();
         horaFinal = scanner.nextInt();
         minutoFinal = scanner.nextInt();
-        if (horaFinal > horaInicial) {
-            horasEMinutosTotais = horaInicial - horaFinal;
-            if (horasEMinutosTotais < 0) {
-                resultadoFinal = horasEMinutosTotais * (-1);
-                System.out.println(resultadoFinal);
+        if (horaFinal > horaInicial && minutoFinal > minutoInicial) {
+            horasTotais = horaInicial - horaFinal;
+            minutosTotais = minutoInicial - minutoFinal;
+            resultadoFinalHoras = horasTotais * (-1);
+            resultadoFinalMinutos = minutosTotais * (-1);
+            if (horasTotais < 0) {
+                resultadoFinalHoras = horasTotais * (-1);
+                resultadoFinalMinutos = minutosTotais * (-1);
+                formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, resultadoFinalMinutos);
+                System.out.println(formatcaoMsg);
+            } else if (minutosTotais < 0) {
+                resultadoFinalHoras = horasTotais * (-1);
+                resultadoFinalMinutos = minutosTotais * (-1);
+                formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, resultadoFinalMinutos);
+                System.out.println(formatcaoMsg);
             }
-        } else if (horaInicial > horaFinal) {
-            horasEMinutosTotais = (horaFinal + 24) - horaInicial;
-            System.out.println(horasEMinutosTotais);
+        } else if (horaInicial > horaFinal && minutoInicial > minutoFinal) {
+            minutosTotais = (minutoFinal + 60) - minutoInicial;
+            horasTotais = (horaFinal + 24) - horaInicial;
+            System.out.println(horasTotais);
         } else {
             System.out.println(24);
 

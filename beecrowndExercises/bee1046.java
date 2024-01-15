c21c55c3fc7c955cddccac946cf8e6dce13cd485
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-15 18:32:18 -0300

    Resolução bee1046

--- beecrowndExercises/bee1046.java
+++ beecrowndExercises/bee1046.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class bee1046 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int horaInicial, horaFinal,horasDeJogo;
+        int horaInicial, horaFinal,horasDeJogo,resultadoFinal;
         horaInicial = scanner.nextInt();
         horaFinal = scanner.nextInt();
-        horasDeJogo=0;
-
         if (horaInicial<horaFinal){
             horasDeJogo = horaInicial-horaFinal;
+            if (horasDeJogo<0){
+                resultadoFinal = horasDeJogo*(-1);
+                System.out.printf("O JOGO DUROU %d HORA(S)\n",resultadoFinal);
+            }
         }else if(horaFinal<horaInicial){
             horasDeJogo = (horaFinal+24)-horaInicial;
+            System.out.printf("O JOGO DUROU %d HORA(S)\n",horasDeJogo);
+        }else {
+            System.out.printf("O JOGO DUROU %d HORA(S)\n",24);
         }
-        System.out.println(horasDeJogo);
+
     }
 }

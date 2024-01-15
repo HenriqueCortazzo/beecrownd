40cb7ef554cf20409e357ddc141083012a744d11
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-15 19:13:06 -0300

    Reformulação de código

--- beecrowndExercises/bee1047.java
+++ beecrowndExercises/bee1047.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class bee1047 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int horaInicial, minutoInicial, horaFinal, minutoFinal, horasTotais, minutosTotais, resultadoFinalHoras, resultadoFinalMinutos, horasEMinutosResultantes;
-        resultadoFinalHoras = 0;
-        resultadoFinalMinutos = 0;
-        String formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, resultadoFinalMinutos);
+        int horaInicial, minutoInicial, horaFinal, minutoFinal, horasTotais, minutosTotais, resultadoFinalHoras, resultadoFinalMinutos;
+        String formatcaoMsg;
         horaInicial = scanner.nextInt();
         minutoInicial = scanner.nextInt();
         horaFinal = scanner.nextInt();
@@ -16,8 +14,6 @@ public class bee1047 {
         if (horaFinal > horaInicial && minutoFinal > minutoInicial) {
             horasTotais = horaInicial - horaFinal;
             minutosTotais = minutoInicial - minutoFinal;
-            resultadoFinalHoras = horasTotais * (-1);
-            resultadoFinalMinutos = minutosTotais * (-1);
             if (horasTotais < 0) {
                 resultadoFinalHoras = horasTotais * (-1);
                 resultadoFinalMinutos = minutosTotais * (-1);
@@ -30,7 +26,6 @@ public class bee1047 {
                 System.out.println(formatcaoMsg);
             }
         } else if (horaInicial > horaFinal && minutoInicial > minutoFinal) {
-            minutosTotais = (minutoFinal + 60) - minutoInicial;
             horasTotais = (horaFinal + 24) - horaInicial;
             System.out.println(horasTotais);
         } else {

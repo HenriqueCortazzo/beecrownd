92234910424b1d708aba9625a6adb234042a574a
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-16 04:51:23 -0300

    Reformulação do código, e resolução do mesmo

--- beecrowndExercises/bee1047.java
+++ beecrowndExercises/bee1047.java
@@ -26,11 +26,34 @@ public class bee1047 {
                 System.out.println(formatcaoMsg);
             }
         } else if (horaInicial > horaFinal && minutoInicial > minutoFinal) {
+            minutosTotais = (minutoFinal + 60) - minutoInicial;
             horasTotais = (horaFinal + 24) - horaInicial;
-            System.out.println(horasTotais);
-        } else {
-            System.out.println(24);
+            formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", horasTotais, minutosTotais);
+            System.out.println(formatcaoMsg);
+        } else if (horaFinal > horaInicial && minutoFinal < minutoInicial) {
+            horasTotais = horaInicial - horaFinal;
+            minutosTotais = (minutoFinal + 60) - minutoInicial;
+            if (minutosTotais == 59) {
+                if (horasTotais < 0) {
+                    resultadoFinalHoras = horasTotais * (-1) - 1;
+                    formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, minutosTotais);
+                    System.out.println(formatcaoMsg);
+                }
+            } else if (horaFinal < horaInicial && minutoFinal > minutoInicial) {
+                horasTotais = (horaFinal + 24) - horaInicial;
+                minutosTotais = minutoInicial - minutoFinal;
+                if (minutosTotais == 59) {
+                    if (horasTotais < 0) {
+                        resultadoFinalHoras = horasTotais * (-1) - 1;
+                        formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", resultadoFinalHoras, minutosTotais);
+                        System.out.println(formatcaoMsg);
+                    }
+                }
+            } else {
+                formatcaoMsg = String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", 24, 0);
+                System.out.println(formatcaoMsg);
 
+            }
         }
     }
 }

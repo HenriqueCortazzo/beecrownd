3ebb772188a6398e21ae578bab1975a96b3f8785
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-18 08:16:36 -0300

    Finalização bee1048

--- beecrowndExercises/bee1048.java
+++ beecrowndExercises/bee1048.java
@@ -14,31 +14,31 @@ public class bee1048 {
             novoSalarioFuncionario = salarioFuncionario + 15 * (salarioFuncionario / 100);
             reajuste = novoSalarioFuncionario - salarioFuncionario;
             percentual = 15;
-            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d%%", novoSalarioFuncionario, reajuste, percentual);
+            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d %%", novoSalarioFuncionario, reajuste, percentual);
             System.out.println(format);
         } else if (salarioFuncionario > 400 && salarioFuncionario <= 800) {
             novoSalarioFuncionario = salarioFuncionario + 12 * (salarioFuncionario / 100);
             reajuste = novoSalarioFuncionario - salarioFuncionario;
             percentual = 12;
-            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d%%", novoSalarioFuncionario, reajuste, percentual);
+            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d %%", novoSalarioFuncionario, reajuste, percentual);
             System.out.println(format);
         } else if (salarioFuncionario > 800 && salarioFuncionario <= 1200) {
             novoSalarioFuncionario = salarioFuncionario + 10 * (salarioFuncionario / 100);
             reajuste = novoSalarioFuncionario - salarioFuncionario;
             percentual = 10;
-            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d%%", novoSalarioFuncionario, reajuste, percentual);
+            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d %%", novoSalarioFuncionario, reajuste, percentual);
             System.out.println(format);
         } else if (salarioFuncionario > 1200 && salarioFuncionario <= 2000) {
             novoSalarioFuncionario = salarioFuncionario + 7 * (salarioFuncionario / 100);
             reajuste = novoSalarioFuncionario - salarioFuncionario;
             percentual = 7;
-            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d%%", novoSalarioFuncionario, reajuste, percentual);
+            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d %%", novoSalarioFuncionario, reajuste, percentual);
             System.out.println(format);
         } else if (salarioFuncionario > 2000) {
             novoSalarioFuncionario = salarioFuncionario + 4 * (salarioFuncionario / 100);
             reajuste = novoSalarioFuncionario - salarioFuncionario;
             percentual = 4;
-            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d%%", novoSalarioFuncionario, reajuste, percentual);
+            format = String.format("Novo salario: %.2f\n" + "Reajuste ganho: %.2f\n" + "Em percentual: %d %%", novoSalarioFuncionario, reajuste, percentual);
             System.out.println(format);
         }
     }

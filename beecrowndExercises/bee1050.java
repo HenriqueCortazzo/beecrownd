79d6f982272bc2e1708d782c6e120ae55e8f9f59
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-22 20:05:51 -0300

    Estabilização de switch

--- beecrowndExercises/bee1050.java
+++ beecrowndExercises/bee1050.java
@@ -7,8 +7,31 @@ public class bee1050 {
         Scanner scanner = new Scanner(System.in);
         int ddd;
         ddd = scanner.nextInt();
-        switch (ddd){
-            
+        switch (ddd) {
+            case 61:
+                System.out.println("Brasilia");
+                break;
+            case 71:
+                System.out.println("Salvador");
+                break;
+            case 11:
+                System.out.println("Sao Paulo");
+                break;
+            case 21:
+                System.out.println("Rio de Janeiro");
+                break;
+            case 32:
+                System.out.println("Juiz de Fora");
+                break;
+            case 19:
+                System.out.println("Campinas");
+                break;
+            case 27:
+                System.out.println("Vitoria");
+                break;
+            case 31:
+                System.out.println("Belo Horizonte");
+                break;
         }
     }
 }

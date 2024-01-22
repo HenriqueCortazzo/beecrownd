bf2396192d9898dd174f8b731f47e53b3069b9cb
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-22 19:13:07 -0300

    Adicionnando variaveis

--- beecrowndExercises/bee1049.java
+++ beecrowndExercises/bee1049.java
@@ -1,7 +1,13 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1049 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String palavra1, palavra2, palavra3;
+        palavra1 = scanner.next();
+        palavra2 = scanner.next();
+        palavra3 = scanner.next();
     }
 }

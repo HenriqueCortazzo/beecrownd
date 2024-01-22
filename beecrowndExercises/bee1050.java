fbdd62a6fbcf84b2192685e1a894dfb48a54ec56
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-22 20:01:53 -0300

    Inicializando variaveis

--- beecrowndExercises/bee1050.java
+++ beecrowndExercises/bee1050.java
@@ -1,7 +1,14 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1050 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int ddd;
+        ddd = scanner.nextInt();
+        switch (ddd){
+            
+        }
     }
 }

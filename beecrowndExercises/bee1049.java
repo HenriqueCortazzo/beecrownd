7b06df665f7f8c660671fbb85f4c8052b8f52a59
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-22 19:27:01 -0300

    Finalização bee1049

--- beecrowndExercises/bee1049.java
+++ beecrowndExercises/bee1049.java
@@ -9,5 +9,33 @@ public class bee1049 {
         palavra1 = scanner.next();
         palavra2 = scanner.next();
         palavra3 = scanner.next();
+
+        if (palavra1.equalsIgnoreCase("vertebrado")) {
+            switch (palavra2) {
+                case "ave":
+                    if (palavra3.equalsIgnoreCase("carnivoro")) {
+                        System.out.println("aguia");
+                    } else System.out.println("pomba");
+                    break;
+                case "mamifero":
+                    if (palavra3.equalsIgnoreCase("onivoro")) {
+                        System.out.println("homem");
+                    } else System.out.println("vaca");
+                    break;
+            }
+        } else if (palavra1.equalsIgnoreCase("invertebrado")) {
+            switch (palavra2) {
+                case "inseto":
+                    if (palavra3.equalsIgnoreCase("hematofago")) {
+                        System.out.println("pulga");
+                    } else System.out.println("lagarta   ");
+
+                case "analideo":
+                    if (palavra3.equalsIgnoreCase("hematofago")) {
+                        System.out.println("sanguessuga");
+                    } else System.out.println("minhoca");
+            }
+        }
+        
     }
 }

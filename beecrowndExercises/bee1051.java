11893001610cca1c61c07be2874a915d3add2648
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-30 06:18:35 -0300

    Refatoração bee105

--- beecrowndExercises/bee1051.java
+++ beecrowndExercises/bee1051.java
@@ -5,58 +5,26 @@ import java.util.Scanner;
 public class bee1051 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double salario, valorSobImposto, valorDoImposto = 0;
+        double salario, valorDoImposto = 0;
+        double limite = 0, restante, restanteImposto, porcentagem18, porcentagem8;
         salario = scanner.nextDouble();
-        if (salario > 0 && salario <= 2000) {
-            System.out.println("Isento");
-        } else if (salario > 2000 && salario <= 3000) {
-            double restante = 0;
-            double limite = 2000;
-            double restanteImposto = 0;
-            valorSobImposto = salario - limite;
-            if (valorSobImposto + 2000 <= limite) {
-                valorDoImposto = valorSobImposto * ((double) 8 / 100);
-            } else if (valorSobImposto + 2000 > limite) {
-                restante = salario - limite;
-                valorDoImposto = (valorSobImposto - restante) * ((double) 8 / 100);
-                if (restante > 0) {
-                    restanteImposto = restante * ((double) 18 / 100);
-                }
-                valorDoImposto += restanteImposto;
-            }
-            System.out.printf("%.2f\n", valorDoImposto);
-        } else if (salario > 3000 && salario <= 4500) {
-            double restante = 0;
-            double limite = 3000;
-            double restanteImposto = 0;
-            valorSobImposto = salario - 2000; // --> isento / sobImposto é onde sera cobrado a porcentagem
-            if (valorSobImposto + 2000 <= limite) {
-                valorDoImposto = valorSobImposto * ((double) 8 / 100);
-            } else if (valorSobImposto + 2000 > limite) {
-                restante = salario - limite;
-                valorDoImposto = (valorSobImposto - restante) * ((double) 8 / 100);
-                if (restante > 0) {
-                    restanteImposto = restante * ((double) 18 / 100);
-                }
-                valorDoImposto += restanteImposto;
-            }
-        } else {
-            double limite = 4500;
-            double restante = salario - limite;
-            double restanteImposto = restante * ((double) 28 / 100);
-            double restanteSalario = salario - restante;
-            double porcentagem18 = 0;
-            double porcentagem8 = 0;
-            if (restanteSalario > 3000) {
-                 porcentagem18 = (restanteSalario-3000) * ((double) 18 / 100);
-                 restanteSalario =3000;
-                valorDoImposto=restanteImposto+porcentagem18;
-            } if (restanteSalario>2000){
-                 porcentagem8 = (restanteSalario-2000) * ((double) 8 / 100);
-            }
-            valorDoImposto=restanteImposto+porcentagem18+porcentagem8;
+        if (salario > 4500) { //salario = 5000
+            limite = 4500;
+            restante = salario - limite;
+            restanteImposto = restante * ((double) 28 / 100);
+            porcentagem18 = 1500 * ((double) 18 / 100);
+            porcentagem8 = 1000 * ((double) 8 / 100);
+            valorDoImposto = restanteImposto + porcentagem18 + porcentagem8;
+            System.out.printf("R$ %.2f\n", valorDoImposto);
+        } else if (salario > 3000) {
+            limite = 3000;
+            restante = salario - limite;
+            restanteImposto = restante * ((double) 18 / 100);
+            porcentagem8 = 1000 * ((double) 8 / 100);
+            valorDoImposto = restanteImposto + porcentagem8;
+            System.out.printf("R$ %.2f\n", valorDoImposto);
         }
-        System.out.printf("R$ %.2f\n", valorDoImposto);
+        //System.out.printf("R$ %.2f\n", valorDoImposto);
     }
 }
 

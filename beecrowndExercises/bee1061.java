3e3c4db13c5c64d43a4d5b80ef8a2d2bcd2b1b0b
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-05 06:55:47 -0300

    Inicializando variaveis e condicionais

--- beecrowndExercises/bee1061.java
+++ beecrowndExercises/bee1061.java
@@ -1,7 +1,13 @@
 package beecrowndExercises;
 
+import java.util.Scanner;
+
 public class bee1061 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String dia, horas, minutos, segundos;
+        dia = scanner.next();
+        dia = dia.replace(" ", "");
+        char diaNumerico = dia.charAt(dia.length() - 1);
     }
 }

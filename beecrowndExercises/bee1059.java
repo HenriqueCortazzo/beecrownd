bb114dcc56d36bd2cffcb77e7ee55ced7b635d8d
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-05 05:32:07 -0300

    Finalização bee1059

--- beecrowndExercises/bee1059.java
+++ beecrowndExercises/bee1059.java
@@ -2,6 +2,10 @@ package beecrowndExercises;
 
 public class bee1059 {
     public static void main(String[] args) {
-        
+        for(int i = 1;i<101;i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }

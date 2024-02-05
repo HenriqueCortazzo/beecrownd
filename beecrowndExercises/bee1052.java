895c51d5c067944604c320f405f358c2b7ca7e24
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-05 05:22:29 -0300

    Estrutura base de switch case

--- beecrowndExercises/bee1052.java
+++ beecrowndExercises/bee1052.java
@@ -1,2 +1,38 @@
-package beecrowndExercises;public class bee1052 {
+package beecrowndExercises;
+
+import java.util.Scanner;
+
+public class bee1052 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mes = scanner.nextInt();
+        switch (mes) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            default:
+                break;
+        }
+    }
 }

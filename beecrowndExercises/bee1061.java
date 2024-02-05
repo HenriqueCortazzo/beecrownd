08f06adff3ab7e7aa85a5ab2d105a50dfffc5b50
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-05 07:22:24 -0300

    Finalização lógica para receber dados

--- beecrowndExercises/bee1061.java
+++ beecrowndExercises/bee1061.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 public class bee1061 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String dia, horas, minutos, segundos;
-        int diaInicio;
+        String dia,data, horasInicio,horasFinal, minutosInicio,minutosFinal, segundosInicio,segundosFinal;
+        int diaInicio,diafinal;
         dia = scanner.nextLine();
         dia = dia.replace(" ", "");
         if (dia.length() > 4) {
-           diaInicio = Integer.parseInt(String.valueOf(dia.substring(dia.length() - 2)));
+            diaInicio = Integer.parseInt(String.valueOf(dia.substring(dia.length() - 2)));
         } else {
-           diaInicio = Integer.parseInt(String.valueOf(dia.charAt(dia.length() - 1)));
+            diaInicio = Integer.parseInt(String.valueOf(dia.charAt(dia.length() - 1)));
         }
         System.out.println(diaInicio);
+        data = scanner.nextLine();
+        data = data.replace(" : ", "");
+        horasInicio = data.substring(0,2);
+        minutosInicio = data.substring(2,4);
+        segundosInicio =data.substring(4,6);
+        System.out.println(horasInicio +" : "+minutosInicio+" : "+segundosInicio);
+
+        dia = scanner.nextLine();
+        dia = dia.replace(" ", "");
+        if (dia.length() > 4) {
+           diafinal = Integer.parseInt(String.valueOf(dia.substring(dia.length() - 2)));
+        } else {
+            diafinal = Integer.parseInt(String.valueOf(dia.charAt(dia.length() - 1)));
+        }
+        System.out.println(diafinal);
+        data = scanner.nextLine();
+        data = data.replace(" : ", "");
+        horasFinal = data.substring(0,2);
+        minutosFinal = data.substring(2,4);
+        segundosFinal =data.substring(4,6);
+        System.out.println(horasFinal +" : "+minutosFinal+" : "+segundosFinal);
     }
 }

f7b7bed5588cf8829fde1472238964c8bfe65670
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-05 05:54:31 -0300

    Start bee1060

---
+++ beecrowndExercises/bee1060.java
@@ -0,0 +1,24 @@
+package beecrowndExercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bee1060 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int valor1,valor2,valor3,valor4,valor5,valor6;
+        ArrayList <Integer> numbers = new ArrayList<>();
+        valor1 = scanner.nextInt();
+        valor2 = scanner.nextInt();
+        valor3 = scanner.nextInt();
+        valor4 = scanner.nextInt();
+        valor5 = scanner.nextInt();
+        valor6 = scanner.nextInt();
+        numbers.add(valor1,valor2);
+        numbers.add(valor3,valor4);
+        numbers.add(valor5,valor6);
+        for (int i = 0; i < numbers.size(); i++) {
+            if ()
+        }
+    }
+}
